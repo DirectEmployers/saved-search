3c25a04232eac25cbcbea405ef064af914248766
Matt DeBoard <matt.deboard@gmail.com>
2011-10-04 11:53:54 -0400

    Rounded out the SavedSearch model. The save() method now calls _make_qs to generate each search substring, then after each of those _make_qs calls complete, it calls _full_qs to generate the self.querystring attribute, which is what will ultimately be passed to Solr.
    I made an implementation decision to bypass the Haystack API and make each SavedSearch model have that querystring attribute, so that it can be passed directly to the address specified in settings.HAYSTACK_SOLR_URL. The other choice was to have SavedSearch instances have a SearchQuerySet object exposed as an attribute, which may turn out to be necessary soon. I'll jump off that bridge when I come to it, though.

--- saved_search/models.py
+++ saved_search/models.py
@@ -8,9 +8,16 @@ class SavedSearch(models.Model):
     """
     Represents the data in a saved search.
 
+    Instead of creating a SearchQuerySet object and filtering thereupon,
+    the save() method here will make available a self.queryset attribute
+    which will be passed directly to settings.HAYSTACK_SOLR_URL.
+
     """
     def __unicode__(self):
-        return '%s :=> query=[]' % self.name
+        # self.querystring will not be available as an attribute until
+        # post-save. If you call __unicode__ pre-save it will throw
+        # an error.
+        return '%s :=> query=[%s]' % (self.name, self.querystring)
 
     name = models.CharField(max_length=100, help_text=("""
                                                        A concise and descriptive
@@ -38,25 +45,60 @@ class SavedSearch(models.Model):
                                         company's job listings. e.g.:
                                         Dental Technician,Office Assistant
                                         """))
+
     def save(self, *args, **kwargs):
-        self.country_qs = self.make_qs('country', self.country.all())
-        self.state_qs = self.make_qs('state', self.state.all())
-        self.city_qs = self.make_qs('city', self.city.all())
-        self.title_qs = self.make_qs('title', self.title.split(','))
-        self.keyword_qs = self.make_qs('text', self.keyword.split(','))
+        super(SavedSearch, self).save(*args, **kwargs)
+        self.country_qs = self._make_qs('country', self.country.all())
+        self.state_qs = self._make_qs('state', self.state.all())
+        self.city_qs = self._make_qs('city', self.city.all())
+        self.title_qs = self._make_qs('title', self.title.split(','))
+        self.keyword_qs = self._make_qs('text', self.keyword.split(','))
+        self.querystring = self._full_qs()
         super(SavedSearch, self).save(*args, **kwargs)
         
-    def country_qs(self, field, params):
+    def _make_qs(self, field, params):
+        """
+        Generates the query string which will be passed to Solr directly.
+
+        """
+        # If no parameter was passed in, immediately dump back out.
+        if not params:
+            return ''
         qs = []
+        # All fields except full-text search, i.e. keyword search, are
+        # sought disjunctively. In other words, if user wants all jobs
+        # in Austin, Dallas and San Antonio, we want to search for all
+        # jobs in Austin OR Dallas OR San Antonio. Otherwise, searching
+        # conjunctively (AND) will result in only those jobs available
+        # in ALL of those cities together. The difference between spoken
+        # English and logical syntax.
+        joinstring = '+OR+'
+        if field == 'text':
+            joinstring = '+AND+'
+            
         for thing in params:
             if field in ('title', 'text'):
-                joinstring = '+AND+'
                 qs.append('%s:%s' % (field, thing))
             else:
-                joinstring = '+OR+'
                 qs.append('%s:%s' % (field, thing.name))
             
         return joinstring.join(qs)
+
+    def _full_qs(self):
+        """
+        Join all the query substrings from various fields into one
+        'master' querystring for passage to Solr.
+        
+        """
+        fields = []
+        qs_attrs = [self.country_qs, self.state_qs, self.city_qs,
+                    self.title_qs, self.keyword_qs]
+        for attr in qs_attrs:
+            if attr:
+                fields.append(attr)
+        # Using conjunction here since we want all job listings returned
+        # to conform to each individual query term.
+        return '+AND+'.join(fields)
         
 
     class Meta:
