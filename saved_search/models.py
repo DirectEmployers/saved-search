55cd5f242f1db69b458408ab32f900e866d86fbb
Matt DeBoard <matt.deboard@gmail.com>
2011-10-06 10:08:18 -0400

    Several changes to models.py to simplify it. First, country state and city attributes were converted from M2M fields to CharFields. This change allows me to calculate the querystring in the model vice the cmoplicated scheme I was moving toward before. Second, I moved the functions that calculate the querystring out of the model admin and back into the model. These will be run on save.

--- saved_search/models.py
+++ saved_search/models.py
@@ -19,11 +19,11 @@ class SavedSearch(models.Model):
                                                        Texas
                                                        """))
     name_slug = models.SlugField(max_length=100, blank=True, null=True)
-    group = models.ForeignKey(Group, blank=True, null=True)
+    group = models.ManyToManyField(Group, blank=True, null=True)
     date_created = models.DateField(auto_now=True)
-    country = models.ManyToManyField(Country, blank=True, null=True)
-    state = models.ManyToManyField(State, blank=True, null=True)
-    city = models.ManyToManyField(City, blank=True, null=True)
+    country = models.CharField(max_length=255, null=True, blank=True)
+    state = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
     keyword = models.CharField(max_length=255, null=True, blank=True,
                                help_text=("""
                                           A comma-separated list of keywords to
@@ -45,11 +45,64 @@ class SavedSearch(models.Model):
         # any dependent URLs do not change.
         if not self.id:
             self.name_slug = slugify(self.name)
-            
+
+        countries = self._make_qs('country', obj.country.all())
+        states = self._make_qs('state', obj.state.all())
+        cities = self._make_qs('city', obj.city.all())
+        keywords = self._make_qs('text', obj.keyword.split(','))
+        titles = self._make_qs('title', obj.title.split(','))
+        self.querystring = self._full_qs(obj, [countries, states, cities,
+                                               keywords, titles])
         super(SavedSearch, self).save(*args, **kwargs)
     
-
+    def _make_qs(self, field, params):
+        """
+        Generates the query string which will be passed to Solr directly.
         
+        """
+        # If no parameter was passed in, immediately dump back out.
+        if not params:
+            return ''
+        qs = []
+        # All fields except full-text search, i.e. keyword search, are
+        # sought disjunctively. In other words, if user wants all jobs
+        # in Austin, Dallas and San Antonio, we want to search for all
+        # jobs in Austin OR Dallas OR San Antonio. Otherwise, searching
+        # conjunctively (AND) will result in only those jobs available
+        # in ALL of those cities together. The difference between spoken
+        # English and logical syntax.
+        joinstring = ' OR '
+        if field == 'text':
+            joinstring = ' AND '
+            
+        for thing in params:
+            if field in ('title', 'text'):
+                qs.append('%s:%s' % (field, thing))
+            else:
+                qs.append('%s:%s' % (field, thing.name))
+
+        return joinstring.join(qs)  
+    
+    def _full_qs(self, instance, fields):
+        """
+        _full_qs(instance, fields)
+        
+        `instance': SavedSearch model instance
+        `fields':   An iterable containing instance attributes you wish to
+        include in the Solr querystring.
+        
+        Join all the query substrings from various fields into one
+        'master' querystring for passage to Solr.
+        
+        """
+        terms = []
+        for attr in fields:
+            if attr:
+                terms.append(attr)
+                
+        # Using conjunction here since we want all job listings returned
+        # to conform to each individual query term.
+        return ' AND '.join(terms)
 
     class Meta:
         verbose_name = 'Saved Search'
