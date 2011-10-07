8cfe1e26a90fbaa66a90b6f874b06583697c75ea
Matt DeBoard <matt.deboard@gmail.com>
2011-10-07 09:23:04 -0400

    Cleaned up override of add_view method on admin

--- saved_search/admin.py
+++ saved_search/admin.py
@@ -83,37 +83,19 @@ class SavedSearchAdmin(admin.ModelAdmin):
         if not self.has_add_permission(request):
             raise PermissionDenied
 
-        ModelForm = SavedSearchForm(user=request.user)
-        formsets = []
+        form = SavedSearchForm(user=request.user)
         if request.method == 'POST':
             form = SavedSearchForm(data=request.POST, user=request.user)
             if form.is_valid():
                 new_object = self.save_form(request, form, change=False)
-                form_validated = True
+                form.save()
+                form.save_m2m()
             else:
                 form_validated = False
                 new_object = SavedSearchForm
-            prefixes = {}
-            for FormSet, inline in zip(self.get_formsets(request),
-                                       self.inline_instances):
-                prefix = FormSet.get_default_prefix()
-                prefixes[prefix] = prefixes.get(prefix, 0) + 1
-                if prefixes[prefix] != 1:
-                    prefix = "%s-%s" % (prefix, prefixes[prefix])
-                formset = FormSet(data=request.POST, files=request.FILES,
-                                  instance=new_object,
-                                  save_as_new=request.POST.has_key("_saveasnew"),
-                                  prefix=prefix,
-                                  queryset=inline.queryset(request))
-                formsets.append(formset)
-            if all_valid(formsets) and form_validated:
-                self.save_model(request, new_object, form, change=False)
-                form.save_m2m()
-                for formset in formsets:
-                    self.save_formset(request, form, formset, change=False)
-
                 self.log_addition(request, new_object)
                 return self.response_add(request, new_object)
+                
         else:
             # Prepare the dict of initial data from the request.
             # We have to special-case M2Ms as a list of comma-separated PKs.
@@ -125,40 +107,20 @@ class SavedSearchAdmin(admin.ModelAdmin):
                     continue
                 if isinstance(f, models.ManyToManyField):
                     initial[k] = initial[k].split(",")
-            form = ModelForm
-            prefixes = {}
-            for FormSet, inline in zip(self.get_formsets(request),
-                                       self.inline_instances):
-                prefix = FormSet.get_default_prefix()
-                prefixes[prefix] = prefixes.get(prefix, 0) + 1
-                if prefixes[prefix] != 1:
-                    prefix = "%s-%s" % (prefix, prefixes[prefix])
-                formset = FormSet(instance=self.model(), prefix=prefix,
-                                  queryset=inline.queryset(request))
-                formsets.append(formset)
 
         adminForm = helpers.AdminForm(form, list(self.get_fieldsets(request)),
-            self.prepopulated_fields, self.get_readonly_fields(request),
-            model_admin=self)
+                                      self.prepopulated_fields,
+                                      self.get_readonly_fields(request),
+                                      model_admin=self)
         media = self.media + adminForm.media
-
-        inline_admin_formsets = []
-        for inline, formset in zip(self.inline_instances, formsets):
-            fieldsets = list(inline.get_fieldsets(request))
-            readonly = list(inline.get_readonly_fields(request))
-            inline_admin_formset = helpers.InlineAdminFormSet(inline, formset,
-                fieldsets, readonly, model_admin=self)
-            inline_admin_formsets.append(inline_admin_formset)
-            media = media + inline_admin_formset.media
-
         context = {
             'title': _('Add %s') % force_unicode(opts.verbose_name),
             'adminform': adminForm,
             'is_popup': request.REQUEST.has_key('_popup'),
             'show_delete': False,
             'media': mark_safe(media),
-            'inline_admin_formsets': inline_admin_formsets,
-            'errors': helpers.AdminErrorList(form, formsets),
+            'inline_admin_formsets': [],
+            'errors': helpers.AdminErrorList(form, []),
             'root_path': self.admin_site.root_path,
             'app_label': opts.app_label,
         }
@@ -169,14 +131,10 @@ class SavedSearchAdmin(admin.ModelAdmin):
     @csrf_protect_m
     @transaction.commit_on_success
     def change_view(self, request, object_id, extra_context=None):
-        import ipdb
-        ipdb.set_trace()
         "The 'change' admin view for this model."
         model = self.model
         opts = model._meta
-
         obj = self.get_object(request, unquote(object_id))
-
         if not self.has_change_permission(request, obj):
             raise PermissionDenied
 
@@ -195,6 +153,7 @@ class SavedSearchAdmin(admin.ModelAdmin):
                                    instance=obj)
             if form.is_valid():
                 form_validated = True
+                # Store the saved but uncommitted form
                 new_object = self.save_form(request, form, change=True)
             else:
                 form_validated = False
