271f1b05f4db920a290936420acd44a01eccb238
Matt DeBoard <matt.deboard@gmail.com>
2011-12-12 17:10:12 -0500

    Address an issue with character encoding & ampersands.

--- saved_search/models.py
+++ saved_search/models.py
@@ -3,6 +3,7 @@ import operator
 from django.contrib.auth.models import Group
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.utils.html import fix_ampersands
 
 from haystack.query import SearchQuerySet, SQ
 
@@ -92,7 +93,7 @@ class SavedSearch(BaseSavedSearch):
         return {'title': [i for i in self.title.split(sep)],
                 'country': [i for i in self.country.split(sep)],
                 'state': [i for i in self.state.split(sep)],
-                'text': [self._drop_chars(t.name) for t in kw],
+                'text': [fix_ampersands(self._drop_chars(t.name)) for t in kw],
                 'city': [i for i in self.city.split(sep)]}
 
     def _drop_chars(self, tag):
